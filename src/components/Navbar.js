b6f453e16171a2b4268f24464f462d894ee325f6
Shikaraman S <83532128+shivkaraman@users.noreply.github.com>
2023-05-30 19:01:30 +0530

    firebase config updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
         <div className='navbar' style={{background: color}}>
             <nav>
                 <NavLink to='/' className='brand'>
-                    <h1>The Tasty Table</h1>
+                <h1>The Tasty Table</h1>
                 </NavLink>
                 <Searchbar/>
                 <NavLink to='/create'>Create recipe</NavLink>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,17 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDdCnEoNwbkw1_nsKL9a2AqQEE1gbXUY-4",
-    authDomain: "recipe-directory-6b00c.firebaseapp.com",
-    projectId: "recipe-directory-6b00c",
-    storageBucket: "recipe-directory-6b00c.appspot.com",
-    messagingSenderId: "26867401507",
-    appId: "1:26867401507:web:79221830cc4f901fa78296",
-    measurementId: "G-47WKBPPY7M"
-};
-
+    apiKey: "AIzaSyACwiJ-dnj7zlqUKVxxAhQNtO0wcRna_94",
+    authDomain: "the-tasty-table-9b54b.firebaseapp.com",
+    projectId: "the-tasty-table-9b54b",
+    storageBucket: "the-tasty-table-9b54b.appspot.com",
+    messagingSenderId: "718658064638",
+    appId: "1:718658064638:web:260cf8b1cfe99e2df015af",
+    measurementId: "G-K7W5DRZ14X"
+  };
+  
 //Initialize firebase
 firebase.initializeApp(firebaseConfig)
 
