bf6ec689aaf14c4d3a24151b2df0fbc8db9b9a7a
Shikaraman S <83532128+shivkaraman@users.noreply.github.com>
2023-05-31 00:32:01 +0530

    firebase config updated

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,14 @@ import 'firebase/firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyACwiJ-dnj7zlqUKVxxAhQNtO0wcRna_94",
-    authDomain: "the-tasty-table-9b54b.firebaseapp.com",
-    projectId: "the-tasty-table-9b54b",
-    storageBucket: "the-tasty-table-9b54b.appspot.com",
-    messagingSenderId: "718658064638",
-    appId: "1:718658064638:web:260cf8b1cfe99e2df015af",
-    measurementId: "G-K7W5DRZ14X"
-  };
-  
+  apiKey: "AIzaSyACwiJ-dnj7zlqUKVxxAhQNtO0wcRna_94",
+  authDomain: "the-tasty-table-9b54b.firebaseapp.com",
+  projectId: "the-tasty-table-9b54b",
+  storageBucket: "the-tasty-table-9b54b.appspot.com",
+  messagingSenderId: "718658064638",
+  appId: "1:718658064638:web:260cf8b1cfe99e2df015af",
+  measurementId: "G-K7W5DRZ14X"
+};
 //Initialize firebase
 firebase.initializeApp(firebaseConfig)
 
